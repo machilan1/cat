0635427e291c7f75d5a8374f4358f3ba884be91f
machilan1 <machilan1@gmail.com>
2023-12-03 18:38:48 +0800

    init

---
+++ apps/api/src/main.ts
@@ -0,0 +1,17 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+}
+
+bootstrap();
